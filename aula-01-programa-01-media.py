cf128516e11f9ab633a8a3fbe17b74a5e4a23abe
Romulo Rosa @romulorosadev <romulorosadev@gmail.com>
2023-08-15 03:19:34 -0300

    aula-01-01 Calcular e Exibir Media

---
+++ aula-01-programa-01-media.py
@@ -0,0 +1,14 @@
+# Programa 01
+# Calcular a média de dois valores
+
+# 1) Declaro duas variavéis com entrada de dados do tipo float
+# 2) Calculo a média e armazeno o valor numa variavel
+# 3) Exibo no console o valor referente ao calculo da média
+
+#1
+inNota1 = float(input("Digite Nota 1: "))
+inNota2 = float(input("Digite Nota 2: "))
+#2
+outMedia = (inNota1 + inNota2)/2
+#3
+print("Média:", outMedia)
